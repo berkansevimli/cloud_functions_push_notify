263a46b2181ec3fce2833fd6bd7ecb39b27b90bd
berkansevimli <elektrotee12@gmail.com>
2022-03-19 19:48:51 +0300

    Firs Commit

---
+++ functions/index.js
@@ -0,0 +1,64 @@
+const functions = require("firebase-functions");
+const admin = require('firebase-admin');
+
+admin.initializeApp(functions.config().functions);
+
+var newData;
+
+
+exports.messageNotification = functions.firestore.document("MessageNotify/{notifyId}").onCreate( async (snapshot, context) => {
+
+    if(snapshot.empty){
+        console.log('NO DEVICES');
+        return;
+    }
+
+
+    newData=snapshot.data();
+    var tokens = [];
+    tokens = newData.receivers;
+    
+
+
+    // admin.firestore.collection("Users").document()
+
+   
+    var payload ={
+
+        notification: {
+            title: newData.sessionTitle,
+            body: newData.senderName +': '+newData.message,
+            sound: 'default',
+        },
+
+        data:{message: newData.message},
+      
+    }
+
+    try {
+        for(t of tokens){
+            const response = await admin.messaging().sendToDevice(t, payload,   {
+                // Required for background/quit data-only messages on iOS
+                contentAvailable: true,
+                // Required for background/quit data-only messages on Android
+                priority: "high",
+              });
+            console.log('notification sended! ' + 'to : '+ t);
+        }
+       
+        
+    } catch (error) {
+
+        console.log('Error: '+error.message);
+        
+    }
+
+})
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
